b7fb9e9de28bf13862ddfd5cbdc8595ed172bb90
Ivan Voronov <ivanvo@tahometer.com>
2021-04-28 12:28:53 +0300

    work with state(add new massage)

--- src/App.js
+++ src/App.js
@@ -5,38 +5,49 @@ import MessageForm from './components/MessageForm';
 import Messages from './components/Messages';
 
 class App extends Component {
-  user ={
-    name:'Ivan Voronov'
+  state = {
+    user:{
+      name:'Ivan Voronov'
+    },
+    messages : [
+      {
+        body: 'Welcome',
+        author: 'Ivan Voronov',
+        avatar: ''
+      },
+      {
+        body: 'Hi',
+        author: 'Slava Link',
+        avatar: ''
+      },
+      {
+        body: 'Cool!',
+        author: 'Vlad Lord',
+        avatar: ''
+      },
+    ]
   }
-
-  messages = [
-    {
-      body: 'Welcome',
-      authorName: 'Ivan Voronov',
-      avatar: ''
-    },
-    {
-      body: 'Hi',
-      authorName: 'Slava Link',
-      avatar: ''
-    },
-    {
-      body: 'Cool!',
-      authorName: 'Vlad Lord',
-      avatar: ''
-    },
-  ]
 
   sendMessage = (message) => {
     console.log(message);
+    this.setState({
+      messages: [
+        ...this.state.messages, 
+        {
+          ...message, 
+          author: this.state.user.name
+        }
+      ]
+    })
+    console.log('[APP] MESSAGES:',this.state.messages);
   }
 
   render(){
 
     return (
       <div id="chat">
-        <Header user={this.user}/>
-        <Messages messages={this.messages}/>
+        <Header user={this.state.user}/>
+        <Messages messages={this.state.messages}/>
         <MessageForm onSubmit={this.sendMessage}/>
       </div>
     );
--- src/components/Message.js
+++ src/components/Message.js
@@ -6,7 +6,7 @@ export default ({ message }) => {
         <div className="message">
             <img src="" alt="" />
             <div className="info">
-              <div className="authorName">{message.authorName}</div>
+              <div className="authorName">{message.author}</div>
               <div className="body">{message.body}</div>
               <div className="time">16:00</div>
             </div>
